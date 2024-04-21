391dc79d70f948e17368006b79712d435f0d094c
bur320 <136945431+bur320@users.noreply.github.com>
2024-04-21 12:44:32 +0500

    .

--- src/data/data.js
+++ src/data/data.js
@@ -1,36 +1,40 @@
 import { AiFillAppstore } from "react-icons/ai";
-import { FaMobile, FaGlobe} from "react-icons/fa";
+import { FaMobile, FaGlobe } from "react-icons/fa";
 import { SiProgress, SiAntdesign } from "react-icons/si";
+import { LiaLinode } from "react-icons/lia";
 
 // Features Data
 export const featuresData = [
   {
     id: 1,
-    title: "Business Stratagy",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    icon: <AiFillAppstore />,
+    title: "Web Development",
+    des: "Dedicated and passionate student with a versatile skill set, demonstrating a commitment to continuous learning and adaptability. Eager to contribute my diverse skills to collaborative and innovative projects",
   },
   {
     id: 2,
-    icon: <AiFillAppstore />,
-    title: "App Development",
+    title: "UI Design",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
   {
     id: 3,
+    icon: <SiAntdesign />,
+    title: "UX Design",
+    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+  },
+
+  {
+    id: 4,
     icon: <SiProgress />,
     title: "SEO Optimisation",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
-  {
-    id: 4,
-    icon: <FaMobile />,
-    title: "Mobile Development",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
-  },
+
   {
     id: 5,
-    icon: <SiAntdesign />,
-    title: "UX Design",
+    // icon: <FaMobile />,
+    icon: <LiaLinode />,
+    title: "Backend Development",
     des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
   },
   {
